dff764d487b99d56fd7da58710da620c77046bb9
Ojash-Adhikari <oja.adhikari@gmail.com>
2025-11-30 19:38:04 +0545

    test

--- src/MouseHandlers.cpp
+++ src/MouseHandlers.cpp
@@ -5,6 +5,7 @@ Glass *glass = nullptr;
 Square *sqr = nullptr;
 LightSource *light = nullptr;
 
+// repo check
 void mouseDown(int button, int state, int x, int y)
 {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
